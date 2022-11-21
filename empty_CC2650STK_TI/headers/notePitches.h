6bbbecaed184bab70972a1980a16520c53db3005
togenn <perala.toni@gmail.com>
2022-11-21 18:08:03 +0200

    rename noteMultiplier

--- empty_CC2650STK_TI/headers/notePitches.h
+++ empty_CC2650STK_TI/headers/notePitches.h
@@ -1,94 +1,94 @@
 // https://github.com/mtrobregado/TI-CC2640R2F-SensorTag
-#define noteMultiplier 2
+#define NOTE_MULTIPLIER 2
 
 #define NOTE_NULL 0
-#define NOTE_B0  31*noteMultiplier
-#define NOTE_C1  33*noteMultiplier
-#define NOTE_CS1 35*noteMultiplier
-#define NOTE_D1  37*noteMultiplier
-#define NOTE_DS1 39*noteMultiplier
-#define NOTE_E1  41*noteMultiplier
-#define NOTE_F1  44*noteMultiplier
-#define NOTE_FS1 46*noteMultiplier
-#define NOTE_G1  49*noteMultiplier
-#define NOTE_GS1 52*noteMultiplier
-#define NOTE_A1  55*noteMultiplier
-#define NOTE_AS1 58*noteMultiplier
-#define NOTE_B1  62*noteMultiplier
-#define NOTE_C2  65*noteMultiplier
-#define NOTE_CS2 69*noteMultiplier
-#define NOTE_D2  73*noteMultiplier
-#define NOTE_DS2 78*noteMultiplier
-#define NOTE_E2  82*noteMultiplier
-#define NOTE_F2  87*noteMultiplier
-#define NOTE_FS2 93*noteMultiplier
-#define NOTE_G2  98*noteMultiplier
-#define NOTE_GS2 104*noteMultiplier
-#define NOTE_A2  110*noteMultiplier
-#define NOTE_AS2 117*noteMultiplier
-#define NOTE_B2  123*noteMultiplier
-#define NOTE_C3  131*noteMultiplier
-#define NOTE_CS3 139*noteMultiplier
-#define NOTE_D3  147*noteMultiplier
-#define NOTE_DS3 156*noteMultiplier
-#define NOTE_E3  165*noteMultiplier
-#define NOTE_F3  175*noteMultiplier
-#define NOTE_FS3 185*noteMultiplier
-#define NOTE_G3  196*noteMultiplier
-#define NOTE_GS3 208*noteMultiplier
-#define NOTE_A3  220*noteMultiplier
-#define NOTE_AS3 233*noteMultiplier
-#define NOTE_B3  247*noteMultiplier
-#define NOTE_C4  262*noteMultiplier
-#define NOTE_C4_1 260*noteMultiplier
-#define NOTE_CS4 277*noteMultiplier
-#define NOTE_D4  294*noteMultiplier
-#define NOTE_DS4 311*noteMultiplier
-#define NOTE_E4  330*noteMultiplier
-#define NOTE_F4  349*noteMultiplier
-#define NOTE_FS4 370*noteMultiplier
-#define NOTE_G4  392*noteMultiplier
-#define NOTE_GS4 415*noteMultiplier
-#define NOTE_A4  440*noteMultiplier
-#define NOTE_AS4 466*noteMultiplier
-#define NOTE_B4  494*noteMultiplier
-#define NOTE_C5  523*noteMultiplier
-#define NOTE_CS5 554*noteMultiplier
-#define NOTE_D5  587*noteMultiplier
-#define NOTE_DS5 622*noteMultiplier
-#define NOTE_E5  659*noteMultiplier
-#define NOTE_F5  698*noteMultiplier
-#define NOTE_FS5 740*noteMultiplier
-#define NOTE_G5  784*noteMultiplier
-#define NOTE_GS5 831*noteMultiplier
-#define NOTE_A5  880*noteMultiplier
-#define NOTE_AS5 932*noteMultiplier
-#define NOTE_B5  988*noteMultiplier
-#define NOTE_C6  1047*noteMultiplier
-#define NOTE_CS6 1109*noteMultiplier
-#define NOTE_D6  1175*noteMultiplier
-#define NOTE_DS6 1245*noteMultiplier
-#define NOTE_E6  1319*noteMultiplier
-#define NOTE_F6  1397*noteMultiplier
-#define NOTE_FS6 1480*noteMultiplier
-#define NOTE_G6  1568*noteMultiplier
-#define NOTE_GS6 1661*noteMultiplier
-#define NOTE_A6  1760*noteMultiplier
-#define NOTE_AS6 1865*noteMultiplier
-#define NOTE_B6  1976*noteMultiplier
-#define NOTE_C7  2093*noteMultiplier
-#define NOTE_CS7 2217*noteMultiplier
-#define NOTE_D7  2349*noteMultiplier
-#define NOTE_DS7 2489*noteMultiplier
-#define NOTE_E7  2637*noteMultiplier
-#define NOTE_F7  2794*noteMultiplier
-#define NOTE_FS7 2960*noteMultiplier
-#define NOTE_G7  3136*noteMultiplier
-#define NOTE_GS7 3322*noteMultiplier
-#define NOTE_A7  3520*noteMultiplier
-#define NOTE_AS7 3729*noteMultiplier
-#define NOTE_B7  3951*noteMultiplier
-#define NOTE_C8  4186*noteMultiplier
-#define NOTE_CS8 4435*noteMultiplier
-#define NOTE_D8  4699*noteMultiplier
-#define NOTE_DS8 4978*noteMultiplier
+#define NOTE_B0  31 * NOTE_MULTIPLIER
+#define NOTE_C1  33 * NOTE_MULTIPLIER
+#define NOTE_CS1 35 * NOTE_MULTIPLIER
+#define NOTE_D1  37 * NOTE_MULTIPLIER
+#define NOTE_DS1 39 * NOTE_MULTIPLIER
+#define NOTE_E1  41 * NOTE_MULTIPLIER
+#define NOTE_F1  44 * NOTE_MULTIPLIER
+#define NOTE_FS1 46 * NOTE_MULTIPLIER
+#define NOTE_G1  49 * NOTE_MULTIPLIER
+#define NOTE_GS1 52 * NOTE_MULTIPLIER
+#define NOTE_A1  55 * NOTE_MULTIPLIER
+#define NOTE_AS1 58 * NOTE_MULTIPLIER
+#define NOTE_B1  62 * NOTE_MULTIPLIER
+#define NOTE_C2  65 * NOTE_MULTIPLIER
+#define NOTE_CS2 69 * NOTE_MULTIPLIER
+#define NOTE_D2  73 * NOTE_MULTIPLIER
+#define NOTE_DS2 78 * NOTE_MULTIPLIER
+#define NOTE_E2  82 * NOTE_MULTIPLIER
+#define NOTE_F2  87 * NOTE_MULTIPLIER
+#define NOTE_FS2 93 * NOTE_MULTIPLIER
+#define NOTE_G2  98 * NOTE_MULTIPLIER
+#define NOTE_GS2 104 * NOTE_MULTIPLIER
+#define NOTE_A2  110 * NOTE_MULTIPLIER
+#define NOTE_AS2 117 * NOTE_MULTIPLIER
+#define NOTE_B2  123 * NOTE_MULTIPLIER
+#define NOTE_C3  131 * NOTE_MULTIPLIER
+#define NOTE_CS3 139 * NOTE_MULTIPLIER
+#define NOTE_D3  147 * NOTE_MULTIPLIER
+#define NOTE_DS3 156 * NOTE_MULTIPLIER
+#define NOTE_E3  165 * NOTE_MULTIPLIER
+#define NOTE_F3  175 * NOTE_MULTIPLIER
+#define NOTE_FS3 185 * NOTE_MULTIPLIER
+#define NOTE_G3  196 * NOTE_MULTIPLIER
+#define NOTE_GS3 208 * NOTE_MULTIPLIER
+#define NOTE_A3  220 * NOTE_MULTIPLIER
+#define NOTE_AS3 233 * NOTE_MULTIPLIER
+#define NOTE_B3  247 * NOTE_MULTIPLIER
+#define NOTE_C4  262 * NOTE_MULTIPLIER
+#define NOTE_C4_1 260 * NOTE_MULTIPLIER
+#define NOTE_CS4 277 * NOTE_MULTIPLIER
+#define NOTE_D4  294 * NOTE_MULTIPLIER
+#define NOTE_DS4 311 * NOTE_MULTIPLIER
+#define NOTE_E4  330 * NOTE_MULTIPLIER
+#define NOTE_F4  349 * NOTE_MULTIPLIER
+#define NOTE_FS4 370 * NOTE_MULTIPLIER
+#define NOTE_G4  392 * NOTE_MULTIPLIER
+#define NOTE_GS4 415 * NOTE_MULTIPLIER
+#define NOTE_A4  440 * NOTE_MULTIPLIER
+#define NOTE_AS4 466 * NOTE_MULTIPLIER
+#define NOTE_B4  494 * NOTE_MULTIPLIER
+#define NOTE_C5  523 * NOTE_MULTIPLIER
+#define NOTE_CS5 554 * NOTE_MULTIPLIER
+#define NOTE_D5  587 * NOTE_MULTIPLIER
+#define NOTE_DS5 622 * NOTE_MULTIPLIER
+#define NOTE_E5  659 * NOTE_MULTIPLIER
+#define NOTE_F5  698 * NOTE_MULTIPLIER
+#define NOTE_FS5 740 * NOTE_MULTIPLIER
+#define NOTE_G5  784 * NOTE_MULTIPLIER
+#define NOTE_GS5 831 * NOTE_MULTIPLIER
+#define NOTE_A5  880 * NOTE_MULTIPLIER
+#define NOTE_AS5 932 * NOTE_MULTIPLIER
+#define NOTE_B5  988 * NOTE_MULTIPLIER
+#define NOTE_C6  1047 * NOTE_MULTIPLIER
+#define NOTE_CS6 1109 * NOTE_MULTIPLIER
+#define NOTE_D6  1175 * NOTE_MULTIPLIER
+#define NOTE_DS6 1245 * NOTE_MULTIPLIER
+#define NOTE_E6  1319 * NOTE_MULTIPLIER
+#define NOTE_F6  1397 * NOTE_MULTIPLIER
+#define NOTE_FS6 1480 * NOTE_MULTIPLIER
+#define NOTE_G6  1568 * NOTE_MULTIPLIER
+#define NOTE_GS6 1661 * NOTE_MULTIPLIER
+#define NOTE_A6  1760 * NOTE_MULTIPLIER
+#define NOTE_AS6 1865 * NOTE_MULTIPLIER
+#define NOTE_B6  1976 * NOTE_MULTIPLIER
+#define NOTE_C7  2093 * NOTE_MULTIPLIER
+#define NOTE_CS7 2217 * NOTE_MULTIPLIER
+#define NOTE_D7  2349 * NOTE_MULTIPLIER
+#define NOTE_DS7 2489 * NOTE_MULTIPLIER
+#define NOTE_E7  2637 * NOTE_MULTIPLIER
+#define NOTE_F7  2794 * NOTE_MULTIPLIER
+#define NOTE_FS7 2960 * NOTE_MULTIPLIER
+#define NOTE_G7  3136 * NOTE_MULTIPLIER
+#define NOTE_GS7 3322 * NOTE_MULTIPLIER
+#define NOTE_A7  3520 * NOTE_MULTIPLIER
+#define NOTE_AS7 3729 * NOTE_MULTIPLIER
+#define NOTE_B7  3951 * NOTE_MULTIPLIER
+#define NOTE_C8  4186 * NOTE_MULTIPLIER
+#define NOTE_CS8 4435 * NOTE_MULTIPLIER
+#define NOTE_D8  4699 * NOTE_MULTIPLIER
+#define NOTE_DS8 4978 * NOTE_MULTIPLIER
